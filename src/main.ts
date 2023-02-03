6fe81fb0943002112a9f3feaa1b3cdf807850e08
patic457 <112005137+patic457@users.noreply.github.com>
2023-02-03 07:46:04 +0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors;
-  app.setGlobalPrefix('api');
+  // app.setGlobalPrefix('api');
+  // app.useGlobalPipes(new ValidationPipe);
   const config = new DocumentBuilder()
     .setTitle('MFAF Package Offering')
     .setDescription('MFAF API description')
@@ -13,7 +15,7 @@ async function bootstrap() {
     .addTag('mfaf')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  // SwaggerModule.setup('api', app, document);
 
   await app.listen(3030);
 }
